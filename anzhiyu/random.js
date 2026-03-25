28eeaab258af01177c620558c9623a0102b145f6
vexovov <vexovov@gmail.com>
2026-03-25 10:51:29 +0800

    Site updated: 2026-03-25 10:51:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/25/hello-world/","2026/03/25/001-Hexo-安装/"];function toRandomPost(){
+var posts=["2026/03/25/001-Hexo-安装/","2026/03/25/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
