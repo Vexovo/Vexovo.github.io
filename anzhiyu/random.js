5b7884bf1ce6b9c61b69177686d1ea8eed545357
vexovov <vexovov@gmail.com>
2026-05-05 13:35:28 +0800

    Site updated: 2026-05-05 13:35:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/25/001_Hexo-安装/","2026/03/26/002_Windows 系统激活/","2026/03/30/004-APK-安装包/","2026/03/26/003_KOF 15 八神庵/"];function toRandomPost(){
+var posts=["2026/03/30/004-APK-安装包/","2026/03/26/003_KOF 15 八神庵/","2026/03/25/001_Hexo-安装/","2026/03/26/002_Windows 系统激活/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
